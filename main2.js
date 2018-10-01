238653f95431c261f708f259c9f21f094b2701eb
David Turner <csusbdt@gmail.com>
2018-09-30 21:41:06 -0700

    X

--- main2.js
+++ main2.js
@@ -1,7 +1,6 @@
 /*
-Project goal: to illustrate object-oriented design in Javascript.
-
-TODO: create 2 or more boxes whose colors can be individually toggled.
+  Project goal: to illustrate object-oriented design in Javascript.
+  TODO: create 2 or more boxes whose colors can be individually toggled.
 */
 
 (function() {
@@ -30,11 +29,15 @@ TODO: create 2 or more boxes whose colors can be individually toggled.
     let dt = (millis - this.previousMillis) / 1000.0;
     this.previousMillis = millis;
     this.x += this.dx * dt;
-    if (this.x > canvas.width) this.x = 0;
+    if (this.x > canvas.width) this.x = -this.w;
+    this.draw();
+    window.requestAnimationFrame(this.anim.bind(this));
+  }
+
+  box.draw = function() {
     ctx.fillStyle = this.color;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillRect(this.x, this.y, this.w, this.h);
-    window.requestAnimationFrame(this.anim.bind(this));
   };
 
   box.click = function(x, y) {
