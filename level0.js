98f48e9881aae395c82e1defae7843e37e1a1bb4
David Turner <csusbdt@gmail.com>
2018-10-11 12:35:26 -0700

    X

--- level0.js
+++ level0.js
@@ -13,15 +13,21 @@
       if (o instanceof app.Enemy && o.isLiving()) win = false;
     }
     if (win) {
-      console.log('WIN!!!');
+      document.body.innerHTML = '';
+      var node = document.createElement("p");
+      var textnode = document.createTextNode("Win!");
+      node.appendChild(textnode);
+      document.body.appendChild(node);
+      setTimeout(function() { window.location = 'level2.html'; }, 2000 );
     }
   }
 
   while (app.objs.length > 0) app.objs.pop();
-  app.objs.push(new app.Enemy( 0, 30, 80, 32,  90));
-  app.objs.push(new app.Enemy(50, 75, 80, 16, 190));
-  app.objs.push(app.player);
-  app.objs.push(ai);
+
+  app.objs.push(new app.Enemy( 0, 30, 80, 32,  90) );
+  app.objs.push(new app.Enemy(50, 75, 80, 16, 190) );
+  app.objs.push(app.player                         );
+  app.objs.push(ai                                 );
 
 })();
 
