19137b1a1f9143ced1dd8a6185edfd91885f6aa8
David Turner <csusbdt@gmail.com>
2018-10-11 11:06:27 -0700

    X

--- enemy.js
+++ enemy.js
@@ -1,19 +1,23 @@
 (function() {
   app.Enemy = function(x, y, w, h, dx) {
-    app.Box.call(this, x, y, w, h, dx, '#ff0000');
-  }
+    app.Box.call(this, x, y, w, h, dx, '#00ff00');
+  };
 
   app.Enemy.prototype = Object.create(app.Box.prototype);
   app.Enemy.prototype.constructor = app.Enemy;
 
+  app.Enemy.prototype.isLiving = function() {
+    return this.color === '#00ff00';
+  };
+
   app.Enemy.prototype.update = function(dt) {
     this.x += this.dx * dt;
     if (this.x > app.canvas.width) this.x = -this.w;
-  }
+  };
 
   app.Enemy.prototype.keydown = function(key) {
     if (key === ' ' && app.player.covers(this)) {
-      this.color = '#00ff00';
+      this.color = '#ff0000';
     }
   };
 
--- level0.js
+++ level0.js
@@ -4,10 +4,24 @@
 
 (function() {
 
+  let ai = new app.Object();
+
+  ai.update = function(dt) {
+    let win = true;
+    for (let i = 0; i < app.objs.length; ++i) {
+      let o = app.objs[i];
+      if (o instanceof app.Enemy && o.isLiving()) win = false;
+    }
+    if (win) {
+      console.log('WIN!!!');
+    }
+  }
+
   while (app.objs.length > 0) app.objs.pop();
   app.objs.push(new app.Enemy( 0, 30, 80, 32,  90));
   app.objs.push(new app.Enemy(50, 75, 80, 16, 190));
   app.objs.push(app.player);
+  app.objs.push(ai);
 
 })();
 
